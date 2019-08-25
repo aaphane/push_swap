602d96621beaed3e2e7405413713f3c76de63ee7
Andries APHANE <aaphane@c4r10s9.wethinkcode.co.za>
2019-08-25 09:28:04 +0200

    aaphane

---
+++ ft_sort_three.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_three.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaphane <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/25 08:00:08 by aaphane           #+#    #+#             */
+/*   Updated: 2019/08/25 08:00:09 by aaphane          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft/libft.h"
+#include "push.h"
+
+
+void		ft_sort_three(t_node **head)
+{
+	t_node *current;
+
+	current = *head;
+
+	if ((current->x < current->next->x) && (current->next->x < current->next->next->x) && (current->next->next->x > current->x))
+		return ;
+	else if ((current->x > current->next->x) && (current->next->x > current->next->next->x) && (current->next->next->x < current->x))
+	{
+		ft_rotate(&current);
+		ft_putendl("ra");
+		ft_swap_nodes(&current);
+		ft_putendl("sa");
+	}
+	else if ((current->x > current->next->x) && (current->next->x < current->next->next->x) && (current->next->next->x > current->x))
+	{
+		ft_swap_nodes(&current);
+		ft_putendl("sa");
+	}
+	else if ((current->x > current->next->x) && (current->next->x < current->next->next->x) && (current->next->next->x < current->x))
+	{
+		ft_rotate(&current);
+		ft_putendl("ra");
+	}
+	else if ((current->x < current->next->x) && (current->next->x > current->next->next->x) && (current->next->next->x < current->x))
+	{
+		ft_reverse_rotate(&current);
+		ft_putendl("rra");
+	}
+	else
+	{
+		ft_reverse_rotate(&current);
+		ft_putendl("rra");
+		ft_swap_nodes(&current);
+		ft_putendl("sa");
+	}
+}
