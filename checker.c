4d56b2c68c6db0750ee278ba961fc039e24c5806
ftv User <ftvuser@ftv-m1-34.local>
2019-08-26 21:18:48 +0200

    aaphane

--- checker.c
+++ checker.c
@@ -15,82 +15,63 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
-	int j;
-	int k;
-	int *namba;
-	char *line;
-	char **temp;
-	t_node *stack_a = NULL;
-	t_node *stack_b = NULL;
+		int i;
+		int j;
+		int k;
+		int *namba;
+		char *line;
+		char **temp;
+		t_node *stack_a = NULL;
+		t_node *stack_b = NULL;
 
-	i = 1;
-	k = 0;
+		i = 1;
+		k = 0;
 
-	namba = (int *)malloc(sizeof(int) * (argc - 1));
-	if (argc > 1)
-	{
-		while (argc > i)
+		namba = (int *)malloc(sizeof(int) * (argc - 1));
+		if (argc > 1)
 		{
-			temp = ft_strsplit(argv[i], ' ');
-			j = 0;
-			while (temp[j])
-			{
-				if (ft_validnumber(temp[j]) == 0 && ft_numboverflow(temp[j]) == 0)
+				while (argc > i)
 				{
-					namba[k] = ft_atoi(temp[j]);
+						temp = ft_strsplit(argv[i], ' ');
+						j = 0;
+						while (temp[j])
+						{
+								if (ft_validnumber(temp[j]) == 0 && ft_numboverflow(temp[j]) == 0)
+										namba[k] = ft_atoi(temp[j]);
+								else
+								{
+										free(namba);
+										exit(0);
+								}
+								j++;
+								k++;
+						}
+						i++;
 				}
-				else
+				if (ft_duplicatenum(namba, k) == -1)
 				{
-					free(namba);
-					exit(0);
+						free(namba);
+						exit(0);
 				}
-				j++;
-				k++;
-			}
-			i++;
-		}
-		if (ft_duplicatenum(namba, k) == -1)
-		{
-			free(namba);
-			exit(0);
-		}
-		i = 0;
-		ft_add_to_stack(namba, k, &stack_a);
-		free(namba);
-		ft_strdel(temp);
-		ft_putendl("\nInput command and use -1 to get outcome outcome:\n");
-		while (get_next_line(0, &line) == 1)
-		{
-			if (ft_validcommand(line) == 0)
-			{
+				i = 0;
+				ft_add_to_stack(namba, k, &stack_a);
+				free(namba);
+				ft_strdel(temp);
+				ft_putendl("\nInput command and use -1 to get outcome outcome:\n");
+				while (get_next_line(0, &line) == 1)
+				{
+						if (ft_validcommand(line) == 0)
+						{
 
-				ft_apply_instructions(line, &stack_a, &stack_b);
-			}
-			else if ((ft_strcmp(line, "-1") == 0))
-			{
+								ft_apply_instructions(line, &stack_a, &stack_b);
+						}
+				}
 				if ((ft_sort_list(&stack_a)) && !(stack_b))
-				{
-					ft_putchar('\n');
-				ft_putstr("\033[;036m");
-					ft_putendl("OK");
-					break ;
-				}
+						ft_putendl("OK");
 				else
-				{
-					ft_putchar('\n');
-				ft_putstr("\033[0;31m");
 					ft_putendl("KO");
-					break ;
-				}
-			}
-			else
-			{
-				ft_putstr("\033[0;31m");
-				ft_putendl("ERROR: Invalid command");
-				break ;
-			}
 		}
-	}
-	return (0);
+		else
+			ft_putendl("ERROR: Invalid command");
+return (0);
 }
